e3c5dbf55fd3ff9effe855306e557f084c669d45
jimbo8399 <jimbo8399@gmail.com>
2016-10-29 16:05:32 +0100

    Part 5 working

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -2,31 +2,34 @@ import { Template } from 'meteor/templating';
 
 import { Tasks } from '../api/tasks.js';
 
+import './task.js';
 import './body.html';
 
 Template.body.helpers({
   tasks() {
-  	return Tasks.find({}, { sort: { createdAt: -1 } });
+    // Show newest tasks at the top
+    return Tasks.find({}, { sort: { createdAt: -1 } });
   },
 });
 
 Template.body.events({
-	'submit .new-task'(event) {
-		//prevent default browser form submit
-		event.preventDefault();
+  'submit .new-task'(event) {
+    // Prevent default browser form submit
+    event.preventDefault();
 
-		//get value from form element
-		const target = event.target;
-		const text = target.text.value;
+    // Get value from form element
+    const target = event.target;
+    const text = target.text.value;
 
-		// insert a task into collection
-		Tasks.insert({
-			text,
-			createdAt: new Date(),
-		});
+    // Insert a task into the collection
+    Tasks.insert({
+      text,
+      createdAt: new Date(), // current time
+    });
 
-		// clear form
-		target.text.value = '';
-	},
+    // Clear form
+    target.text.value = '';
+  },
 });
+
 

226b2b15c9ccf235ccca3304489f8afacf6238b7
jimbo8399 <jimbo8399@gmail.com>
2016-10-29 15:19:21 +0100

    Part 4 working

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -1,12 +1,32 @@
 import { Template } from 'meteor/templating';
 
+import { Tasks } from '../api/tasks.js';
+
 import './body.html';
 
 Template.body.helpers({
-  tasks: [
-    { text: 'This is task 1' },
-    { text: 'This is task 2' },
-    { text: 'This is task 3' }
-  ]
+  tasks() {
+  	return Tasks.find({}, { sort: { createdAt: -1 } });
+  },
+});
+
+Template.body.events({
+	'submit .new-task'(event) {
+		//prevent default browser form submit
+		event.preventDefault();
+
+		//get value from form element
+		const target = event.target;
+		const text = target.text.value;
+
+		// insert a task into collection
+		Tasks.insert({
+			text,
+			createdAt: new Date(),
+		});
+
+		// clear form
+		target.text.value = '';
+	},
 });
 
